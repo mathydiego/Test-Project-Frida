61097d442c955233b537ab498156b58f0505857b
DaraLevant <daralevant@gmail.com>
2022-05-25 23:09:37 -0500

    Agrege Mosho Apretame :3

--- Assets/Scripts/MoshoApretame.cs
+++ Assets/Scripts/MoshoApretame.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
-public class PressMe : MonoBehaviour
+public class MoshoApretame : MonoBehaviour
 {
-    int n;
+   
     public Text myText;
     public void OnButtonPress()
     {
-        n++;
-        myText.text = "Te adoyo mucho";
+        myText.text = "mi meia Lucuma :3";
     }
+
+
+
+
+
+
+
+
+
+
 
 }
